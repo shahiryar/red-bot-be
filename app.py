066061014dbcf701298308106ac24e515af4128b
shahiryar <shahiryarsaleem@gmail.com>
2024-12-07 09:51:49 +0500

    Initialise stateful chatbot api

---
+++ app.py
@@ -0,0 +1,47 @@
+from flask import Flask, session, request, jsonify
+from flask_session import Session
+import os
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'tell a lie, live the truth'
+app.config['SESSION_TYPE'] = 'filesystem'
+
+Session(app)
+
+
+@app.route('/start', methods=['GET'])
+def start_session():
+    session['user_id'] = os.urandom(8).hex()
+    session['history'] = []
+
+    return jsonify({"message": "Session started!",
+                     "session_id": session['user_id']
+                     }), 200
+
+
+def bot_respond(user_message):
+    bot_response = f"user said, {user_message}"
+    return bot_response
+
+@app.route('/message', methods=['POST'])
+def handle_message():
+    user_message = request.json.get('message')
+    if 'user_id' not in session:
+        return jsonify({"error": "No active session"}), 400
+    
+    session['history'].append({"user": user_message})
+    bot_reply = bot_respond(user_message)
+    session['history'].append({'bot': bot_reply})
+
+    return jsonify({"reply": bot_reply,
+                    "history": session['history']
+                    }), 200
+
+@app.route('/end', methods=['GET'])
+def end_session():
+    session.clear()
+    return jsonify(message="Session ended!"), 200
+
+if __name__ == '__main__':
+    app.run(debug=True)
